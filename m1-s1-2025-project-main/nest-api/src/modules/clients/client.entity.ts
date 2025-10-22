ebff85b9b5b37b616739d871a6efaaba131db6b1
Alexis BOULANGER <alexis.boulanger@student.junia.com>
2025-10-22 09:43:06 +0200

    Création de la table Client

---
+++ m1-s1-2025-project-main/nest-api/src/modules/clients/client.entity.ts
@@ -0,0 +1,21 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export type ClientId = string & { __brand: 'Client' };
+
+@Entity('clients')
+export class ClientEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: ClientId;
+
+  @Column({ name: 'name', type: 'varchar' })
+  name: string;
+
+  @Column({ name: 'first_name', type: 'varchar' })
+  firstName: string;
+
+  @Column({ name: 'email', type: 'varchar', nullable: true })
+  email?: string;
+
+  @Column({ name: 'photo_url', type: 'varchar', nullable: true })
+  photoUrl?: string;
+}
